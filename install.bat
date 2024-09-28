f504154b4cd3e59f99f376a0a19b7b2b7fd1a2ba
Celentroft <scarlxrd_1337@cianigger.com>
2024-09-28 15:42:58 +0200

    update

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo off
+pip install - requirements.txt
+pause
---
+++ wl.py
@@ -0,0 +1,6 @@
+# Here enter all id you don't want message with bot
+
+WHITELIST = {
+    1289465975791419445,
+    940965110443302974
+}
